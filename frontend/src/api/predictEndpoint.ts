3f6061d338ef64d8ea73ca5837ee18f3f5d476b4
Yue <yue@potentia.asia>
2023-03-13 14:46:25 +0800

    remove frontend submodule

--- frontend/src/api/predictEndpoint.ts
+++ frontend/src/api/predictEndpoint.ts
@@ -1,6 +1,6 @@
-import http from 'src/celestial-ui/util/http';
 import { PostPredictRequest } from 'src/model/Api';
 import { ReplicateResponse } from 'src/model/Replicate';
+import http from 'src/util/http';
 
 const postPredict = async (data: PostPredictRequest, userId: string) =>
   await http.post<ReplicateResponse>('predict', {
